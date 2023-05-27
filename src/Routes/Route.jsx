99b64aa310cd9e277dcbfadbafffee0073ef6462
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-05-28 02:05:25 +0600

    implement captcha method

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -5,6 +5,7 @@ import Home from "../Pages/Home/Home/Home";
 import Main from "../Layouts/Main";
 import MenuPage from "../Pages/MenuPage/MenuPage";
 import Order from "../Pages/Order/Order";
+import Login from "../Pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
             {
                 path: '/order/:category',
                 element: <Order></Order>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     },
